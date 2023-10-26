d3b78ed00f929a43d226925cdeb20b974cb7dcbf
RammeDon <raminkhareji@gmail.com>
2023-10-26 17:50:23 +0200

    Adding navigation for privacy policy and About Us page with their child pages

--- themes/fictional-university-theme/functions.php
+++ themes/fictional-university-theme/functions.php
@@ -7,3 +7,9 @@ function university_files() {
     wp_enqueue_style("university_main_styles", get_theme_file_uri("/build/style-index.css"));
 }
 add_action("wp_enqueue_scripts", "university_files");
+
+function university_features() {
+    add_theme_support("title-tag");
+}
+
+add_action("after_setup_theme", "university_features");
